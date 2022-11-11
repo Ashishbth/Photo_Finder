a20d24bbe44926baa8928aa7655c623b45675a3a
Ashish Kumar <ashishbth@gmail.com>
2022-11-11 23:56:31 +0530

    first commit

--- src/component/ImageList.js
+++ src/component/ImageList.js
@@ -6,7 +6,7 @@ const ImageList = (props)=>{
     console.log(props.images);
 
     const images = props.images.map((image)=>{
-        return <ImageCard key={image.id} image={image}/>
+        return <ImageCard key={image.id} image={image}/> 
     })
     return (
         <>
